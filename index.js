cede340b0109787088e0f52b65978ca9cb61c425
Jishen Lin <minmuslin@outlook.com>
2024-02-14 17:46:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,17 +20,26 @@ const readme = `\
   <img src="https://profile-counter.glitch.me/MinmusLin/count.svg" height="30"/>
   visitor!
 </h1>
+
 * 🚀 I'm Jishen Lin, an undergraduate student of School of Software Engineering, Tongji University.
+
 * 🌱 I’m currently learning software engineering and exploring new technologies.
+
 * 🔭 I love collaborating on interesting tech projects and connecting with like-minded individuals.
+
 * 📫 Email: minmuslin@outlook.com
+
 * 💬 WeChat: MinmusLin
+
 <div>
   <img src="https://github-readme-stats.vercel.app/api?username=MinmusLin&show_icons=true&theme=tokyonight&count_private=true" height="190"/>
   <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MinmusLin&theme=tokyonight&layout=compact" height="190"/>
 </div>
+
 ---
+
 ⏳ Year Progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ⏰ Updated on ${new Date().toUTCString()}\
 `
 
