9da199946fcb29f473a66b60ba9c438dcc584d70
Jishen Lin <minmuslin@outlook.com>
2024-02-14 17:01:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,24 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
-
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-
+<h1>
+  <img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" height="30"/>
+  Hi there my No.
+  <img src="https://profile-counter.glitch.me/MinmusLin/count.svg" height="30"/>
+  visitor!
+</h1>
+* 🚀 I'm Jishen Lin, an undergraduate student of School of Software Engineering, Tongji University.
+* 🌱 I’m currently learning software engineering and exploring new technologies.
+* 🔭 I love collaborating on interesting tech projects and connecting with like-minded individuals.
+* 📫 Email: minmuslin@outlook.com
+* 💬 WeChat: MinmusLin
+<div>
+  <img src="https://github-readme-stats.vercel.app/api?username=MinmusLin&show_icons=true&theme=tokyonight&count_private=true" height="190"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MinmusLin&theme=tokyonight&layout=compact" height="190"/>
+</div>
 ---
-
+⏳ Year Progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ⏰ Updated on ${new Date().toUTCString()}
-
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
-`
+\`
 
 console.log(readme)
