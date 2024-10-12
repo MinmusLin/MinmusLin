2d1f717cdbdae8eb9eb16601a43c712fbdf62948
Jishen Lin <minmuslin@outlook.com>
2024-10-13 02:18:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,21 @@ const readme = `\
 
 ### 👨‍💻 About Me
 
-I’m Jishen Lin, an undergraduate student majoring in Software Engineering with a focus on Machine Intelligence at the School of Computer Science and Technology, [Tongji University](https://www.tongji.edu.cn). I am passionate about full-stack development and committed to practicing elegant engineering solutions. I enjoy collaborating with diverse teams to explore cutting-edge technologies and solve real-world problems through innovative and efficient solutions. With experience in full-stack development, AI research, and open-source projects, I am always seeking new challenges and opportunities to learn and grow.
+* I’m Jishen Lin, an undergraduate student majoring in Software Engineering with a focus on Machine Intelligence at the School of Computer Science and Technology, [Tongji University](https://www.tongji.edu.cn).
 
-Feel free to reach out if you’d like to discuss ideas or explore new possibilities in tech innovation!
+* I am passionate about full-stack development and committed to practicing elegant engineering solutions.
 
-[Click here](https://minmuslin.github.io) to visit my homepage!
+* I enjoy collaborating with diverse teams to explore cutting-edge technologies and solve real-world problems through innovative and efficient solutions.
+
+* With experience in full-stack development, AI research, and open-source projects, I am always seeking new challenges and opportunities to learn and grow.
+
+* Feel free to reach out if you’d like to discuss ideas or explore new possibilities in tech innovation! [Click here](https://minmuslin.github.io) to visit my homepage!
 
 ### 🔬 Technical Skills
 
-* 💻 **Programming Languages**: C/C++, C#, Java, Python, HTML, CSS, JavaScript, TypeScript, Dart, SQL, Rust
+* 💻 **Programming Languages**: C/C++, C#, Java, Python, HTML, CSS, JavaScript, TypeScript, Dart, SQL, Swift, Rust
 
-* 🧰 **Frameworks and Libraries**: Node, Vue, Spring Boot, Cocos2d-x, Qt, Flutter, OpenCV
+* 🧰 **Frameworks and Libraries**: Node, Vue, Spring Boot, Cocos2d-x, Qt, Flutter, OpenCV, SwiftUI
 
 * 📈 **Data Analysis**: PyTorch, TensorFlow, Scikit-learn, NumPy, Matplotlib
 
