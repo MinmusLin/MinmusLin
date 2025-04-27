e4c856012e6ed5842fb6c3a0ae09d177def96939
Jishen Lin <minmuslin@outlook.com>
2025-04-27 11:46:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,14 +27,10 @@ const readme = `\
 
 I'm Jishen Lin, an undergraduate student majoring in Software Engineering at [School of Computer Science and Technology](https://cs.tongji.edu.cn), [Tongji University](https://www.tongji.edu.cn). I am passionate about full-stack development and committed to practicing elegant engineering solutions. I enjoy collaborating with diverse teams to explore cutting-edge technologies and solve real-world problems through innovative and efficient solutions. With experience in full-stack development, AI research, and open-source projects, I am always seeking new challenges and opportunities to learn and grow.
 
-### 📊 My GitHub
-
 <div align='center'>
   <img src='https://github-readme-stats.vercel.app/api?username=MinmusLin&show_icons=true&count_private=true' height='190'/>
   <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=MinmusLin&layout=compact' height='190'/>
 </div>
-
-### 🥰 Take a look at my repositories and let's get in touch!\
 `
 
 console.log(readme)
