c747004da3d14290c43d48b8000de963d4211eef
Jishen Lin <minmuslin@outlook.com>
2025-05-08 22:39:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const readme = `\
 
 ### 👨‍💻 About Me ([Blog](https://www.minmuslin.cn), [CV](https://raw.githubusercontent.com/MinmusLin/MinmusLin.github.io/main/docs/public/cv/cv.pdf))
 
-💻 Full-stack development enthusiast passionate about solving real-world problems through elegant engineering practices
-🛒 Backend Software Engineer Intern @ Douyin E-Commerce, ByteDance (ByteIntern Program)
-🎓 B.Eng. in Software Engineering @ School of Computer Science and Technology, Tongji University
-🔍 Thrives on technical challenges and collaborative problem-solving to create impactful solutions
-🌱 Continuously learning and growing, with endless curiosity about emerging technologies and best practices
+* 💻 Full-stack development enthusiast passionate about solving real-world problems through elegant engineering practices
+* 🛒 Backend Software Engineer Intern @ Douyin E-Commerce, ByteDance (ByteIntern Program)
+* 🎓 B.Eng. in Software Engineering @ School of Computer Science and Technology, Tongji University
+* 🔍 Thrives on technical challenges and collaborative problem-solving to create impactful solutions
+* 🌱 Continuously learning and growing, with endless curiosity about emerging technologies and best practices
 
 <div align='center'>
   <img src='https://github-readme-stats.vercel.app/api?username=MinmusLin&show_icons=true&count_private=true' height='190'/>
